c0d4f466414491ef7ab836e54e9ad5a21465cf34
Robert J Magaña <robjmagana@gmail.com>
2020-12-07 17:49:36 -0800

    Created React Project for Tutorial Purposes

---
+++ src/components/app.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class App extends component {
+    state = { } 
+    render() {
+        return (
+            <h1>Our React App Using a Component</h1>
+        );
+    }
+}
+
+export default App;
