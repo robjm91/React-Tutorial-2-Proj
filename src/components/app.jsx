4525f4766c073ed3e2631e3e17fce438a0aa3529
Robert J Magaña <robjmagana@gmail.com>
2020-12-07 19:39:14 -0800

    Edited render method
    added spacing. nothing serious

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import TitleBar from "./TitleBar/titleBar";
 
-class App extends component {
+class App extends Component {
     state = { } 
     render() {
         return (
             <div className="container-fluid">
                 <TitleBar />
+
             </div>
         );
     }
