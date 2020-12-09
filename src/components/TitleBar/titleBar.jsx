2ec3e98770e0ff48fe27602e6e5bdb30cc0f38c7
Robert J Magaña <robjmagana@gmail.com>
2020-12-09 15:38:16 -0800

    imported css styles to title bar jsx file and also changed color of nav bar to black :D

--- src/components/TitleBar/titleBar.jsx
+++ src/components/TitleBar/titleBar.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import "./titleBar.css";
 
 function TitleBar(props) {
     return (
         <div className="row row-spacer">
             <div className="col-md-12" style={{padding: 0}}>
-                <div> 
+                <div className="titlebar-nav"> 
                     <h1>Robert's Library</h1>
                 </div>
             </div>
