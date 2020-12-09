5435c8359b2d345dcbbbca0457b1411ef9d7fefd
Robert J Magaña <robjmagana@gmail.com>
2020-12-09 15:50:19 -0800

    Update app.js

--- src/components/app.js
+++ src/components/app.js
@@ -2,7 +2,19 @@ import React, { Component } from "react";
 import TitleBar from "./TitleBar/titleBar";
 
 class App extends Component {
-    state = { } 
+    constructor (props) {
+        super (props);
+        this.books = [
+            {title: "Ready Player One", author: "Ernest Cline"},
+            {title: "All the Light We Cannot See", author: "Anthony Doerr"},
+            {title: "The First and Last Freedom", author: "Jiddu Krishnamurti"},
+        ];
+        this.sate = {
+            bookNumber: 0
+        }
+
+    }
+     
     render() {
         return (
             <div className="container-fluid">
