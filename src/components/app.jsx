12f8bda9d20773c1d557cf738b560808de8466df
Robert J Magaña <robjmagana@gmail.com>
2020-12-09 18:34:02 -0800

    imported book viewer and set the components attributes to point to appropriate functions

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import TitleBar from "./TitleBar/titleBar";
+import BookViewer from "./BookViewer/bookViewer";
 
 class App extends Component {
     constructor (props) {
@@ -44,6 +45,7 @@ class App extends Component {
         return (
             <div className = "container-fluid">
                 <TitleBar />
+                <BookViewer book={this.books[this.state.bookNumber]} nextBook={() => this.goToNextBook()} previousBook = {() => this.goToPreviousBook()}/>
             </div>
         );
     }
