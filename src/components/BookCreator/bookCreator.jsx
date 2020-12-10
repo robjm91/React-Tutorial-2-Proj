25c0f6342c34a597c49aa0642de38e7e22818047
Robert J Magaña <robjmagana@gmail.com>
2020-12-09 20:32:47 -0800

    added input tag for author stateful variable

--- src/components/BookCreator/bookCreator.jsx
+++ src/components/BookCreator/bookCreator.jsx
@@ -33,6 +33,11 @@ class BookCreator extends Component {
                             onChange={/*Call a function to handle title input change*/}/>
                         </div>
                         <div className="col-md-4">
+                            <label>Author:</label>
+                            <input type="text" name="author" value={this.state.author}
+                            onChange={/*Call a function to handle author input change*/}/>
+                        </div>
+                        <div className="col-md-4">
                             <input type="submit" value="Add"/>
                         </div>
                     </div>
