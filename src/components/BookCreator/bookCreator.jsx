f4d282b89db51a1630ef6fee53cd27677bb993ab
Robert J Magaña <robjmagana@gmail.com>
2020-12-09 19:49:56 -0800

    Created a form in book creator component

--- src/components/BookCreator/bookCreator.jsx
+++ src/components/BookCreator/bookCreator.jsx
@@ -4,9 +4,26 @@ class BookCreator extends Component {
     constructor(props) {
         super(props); 
         this.state = {}
+            title: "",
+            author: ""
     }
     render () {
-        return ();
+        return (
+            <div>
+                <form onSubmit={/*Call a function to hand form submission*/}>
+                    <div className="row col-align">
+                        <div className="col-md-4">
+                            <label>Title:</label>
+                            <input type="text" name="title" value={this.state.title}
+                            onChange={/*Call a function to handle title input change*/}/>
+                        </div>
+                        <div className="col-md-4">
+                            <input type="submit" value="Add"/>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        );
     }
 }
 
