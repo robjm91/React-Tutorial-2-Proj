f78f70dc176d78416a7ba36cdb2e0c40eddd9835
Robert J Magaña <robjmagana@gmail.com>
2020-12-10 16:43:18 -0800

    created footer functional component

---
+++ src/components/Footer/footer.jsx
@@ -0,0 +1,13 @@
+import { waitForElementToBeRemoved } from "@testing-library/react";
+import React from "react";
+import "./footer.css";
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <h4>Copyright @ 2020</h4>
+        </footer>
+    )
+}
+
+export default Footer;
